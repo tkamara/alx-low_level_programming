69e2c27826d8a4a25af43aa6ba8ec412d28b6b47
Chebet Kamara <chebetkamara@gmail.com>
2022-09-08 05:17:28 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,16 @@
 /**
  * Prints string to standard output
+ *
+ * main - entry point of code
+ *
+ * return: zero
  */
 
 #include <stdio.h>
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 
