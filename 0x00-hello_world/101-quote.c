19b5ef0b9d3f17bd49f8a24841a21be13ca3fbbe
Chebet Kamara <chebetkamara@gmail.com>
2022-09-08 08:36:30 -0700

    Printing to standard output

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - printing statement to standard output
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (0);
+}
