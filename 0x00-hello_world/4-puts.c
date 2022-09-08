3f98b9e8a4275aab2a050dfe575bb7d9ded417af
Chebet Kamara <chebetkamara@gmail.com>
2022-09-08 05:03:14 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
+/**
+ * Prints string to standard output
+ */
+
 #include <stdio.h>
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle \n");
+
 	return (0);
+
 }
