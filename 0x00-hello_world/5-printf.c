52943866d1d5097f45f5421aefe07d0c2c71e8fd
Chebet Kamara <chebetkamara@gmail.com>
2022-09-08 07:25:01 -0700

    Using printf to print to std. output

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/**
+ * main -using printf to print to standard output
+ *
+ * Return: zero
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+
+}
