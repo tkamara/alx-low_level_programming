22eef9992ca2bf1904e46befd8111bfce047e179
Chebet Kamara <chebetkamara@gmail.com>
2022-09-09 10:27:02 -0700

    Printing numbers and characters using putchar()

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - printing digits using putchar() and separated by commas
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar((i) + '0');
+		putchar(44);
+		putchar(32);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
