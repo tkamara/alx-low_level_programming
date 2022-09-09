7731787345626e5c922e8b6a8af4afc3acff6c03
Chebet Kamara <chebetkamara@gmail.com>
2022-09-09 08:01:01 -0700

    Determining whether last digit is positive or not

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - function determines if last digit is greater than 5 or less than 6
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int n, rem;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+        rem = n % 10;     
+	if (rem > 5)
+	{
+		printf("The last digit of %d is %d and is greater than 5\n", n, rem);
+	}
+	else if (rem > 0 && rem < 6)
+	{
+		printf("The last digit of %d is %d and is less than 6 and not 0\n", n, rem);
+	}
+	else if (rem == 0)
+	{
+		printf("The last digit of %d is %d and is 0\n", n, rem);
+	}
+
+	return (0);
+}
