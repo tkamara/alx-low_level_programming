7a8b10255e3e77d36bd7e8359cd26186a87c8c44
Chebet Kamara <chebetkamara@gmail.com>
2022-09-09 10:09:44 -0700

    Printing hexadecimal numbers using putchar()

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print hexadecimal numbers for 0-16
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar((i) + '0');
+	}
+	for (ch = 97; ch < 103; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(ch);
+
+	return (0);
+}
