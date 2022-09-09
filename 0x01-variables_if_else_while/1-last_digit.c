342f2e1a341da67a7ccc65dfe13b8f03b762d11e
Chebet Kamara <chebetkamara@gmail.com>
2022-09-09 08:10:33 -0700

    Determining whether last digit is positive or not

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,18 +15,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-        rem = n % 10;     
+	rem = n % 10;
 	if (rem > 5)
 	{
-		printf("The last digit of %d is %d and is greater than 5\n", n, rem);
+	printf("Last digit of %d is %d and is greater than 5\n", n, rem);
 	}
-	else if (rem > 0 && rem < 6)
+	else if (rem != 0 && rem < 6)
 	{
-		printf("The last digit of %d is %d and is less than 6 and not 0\n", n, rem);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, rem);
 	}
 	else if (rem == 0)
 	{
-		printf("The last digit of %d is %d and is 0\n", n, rem);
+	printf("Last digit of %d is %d and is 0\n", n, rem);
 	}
 
 	return (0);
