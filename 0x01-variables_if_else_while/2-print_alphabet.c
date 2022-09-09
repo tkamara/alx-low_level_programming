9c4df0ef669d74e3dcda682dfcbf37aa8c1b5fd2
Chebet Kamara <chebetkamara@gmail.com>
2022-09-09 08:23:35 -0700

    Printing using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet using ASCII
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 97; ch < 123; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
