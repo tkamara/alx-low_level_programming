1769a43f6ea760971f27178bf01291f04bf38ece
Chebet Kamara <chebetkamara@gmail.com>
2022-09-14 09:12:14 -0700

    Summing two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @c: integer to add
+ * @d: integer to add
+ *
+ * Return: sum
+ */
+
+int add(int c, int d)
+{
+	int sum;
+
+	sum = c + d;
+
+	return (sum);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -27,6 +27,8 @@ int _abs(int);
 
 int print_last_digit(int);
 
+int add(int, int);
+
 
 
 
