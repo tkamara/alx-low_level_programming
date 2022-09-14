eaa76f140870a1125c68c214c747233ff4becb14
Chebet Kamara <chebetkamara@gmail.com>
2022-09-14 04:46:47 -0700

    Check if lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - checks if letter is lowercase
+ *
+ * @c: integer to be checked
+ *
+ * Return: 0 if lowercase, 1 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c < '97' && c > '123')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
