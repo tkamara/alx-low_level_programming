e74b681136d2d9ed3f56348545cd02c5e63c5d2d
Chebet Kamara <chebetkamara@gmail.com>
2022-09-14 05:07:28 -0700

    Correction

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,17 +5,17 @@
  *
  * @c: integer to be checked
  *
- * Return: 0 if lowercase, 1 otherwise
+ * Return: 1 if lowercase, 0 otherwise
  */
 
 int _islower(int c)
 {
-	if (c < 'a' && c > 'z')
+	if (c >= 'a' && c <= 'z')
 	{
-		return (0);
+		return (1);
 	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 }
