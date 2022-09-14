b9bc2c9242752e4f94cc5fd1fddede21231ed0de
Chebet Kamara <chebetkamara@gmail.com>
2022-09-14 08:23:57 -0700

    Obtaining absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _abs - function to print absolute value of integer
+ *
+ * @int: integer to be checked
+ *
+ * Return: integer
+ */
+
+int _abs(int)
+{
+	int a;
+
+	if (a < 0)
+	{
+		int b;
+
+		b = a * -1;
+		return (b);
+	}
+	else
+	{
+		return (a);
+	}
+}
