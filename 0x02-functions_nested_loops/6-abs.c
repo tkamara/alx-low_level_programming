e6296f776243497a97a91c2c5a3627e869d471d3
Chebet Kamara <chebetkamara@gmail.com>
2022-09-14 08:38:32 -0700

    Obtaining absolute value of integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,20 +3,18 @@
 /**
  * _abs - function to print absolute value of integer
  *
- * @int: integer to be checked
+ * @a: integer to be checked
  *
  * Return: integer
  */
 
-int _abs(int)
+int _abs(int a)
 {
-	int a;
-
 	if (a < 0)
 	{
 		int b;
 
-		b = a * -1;
+		b = a * (-1);
 		return (b);
 	}
 	else
