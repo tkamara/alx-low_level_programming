c75b89d346f50aeb39de037ea8c2196c17941ac8
Chebet Kamara <chebetkamara@gmail.com>
2022-09-15 00:01:13 -0700

    Debugger for postive and negative numbers

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - debugger
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ *positive_or_negative - check if number is positive or negative
+ *@i: integer to be checked
+ *
+ * Return: none
+ */
+
+void positive_or_negative(int i);
+
+#endif
