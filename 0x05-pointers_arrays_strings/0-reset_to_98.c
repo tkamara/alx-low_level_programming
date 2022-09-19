49cbe611f6a37f5bf11d862b9c03ba994510bbdb
Chebet Kamara <chebetkamara@gmail.com>
2022-09-19 00:18:08 -0700

    Updating value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value it points to to 98
+ * @n: the pointer
+ *
+ * Return: none
+ */
+
+void reset_to_98(int *n)
+{
+	n = 98;
+}
+
+
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * reset_to_98 - change value of number to 98
+ * @*n: pointer to n
+ *
+ * Return: none
+ */
+
+void reset_to_98(int *n)
+
+
+#endif
