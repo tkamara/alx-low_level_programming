38c0f0335b24a510de39e6b79fbda658f5ad264a
Chebet Kamara <chebetkamara@gmail.com>
2022-09-19 00:03:42 -0700

    Pointers and strings

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - to check code
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	print_triangle(2);
+	print_triangle(0);
+	print_triangle(10);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	print_diagonal(0);
+	print_diagonal(10);
+	return (0);
+}
