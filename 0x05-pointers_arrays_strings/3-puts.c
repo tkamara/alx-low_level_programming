f8125b12a5ab19b6d98c82aa19c1346020fdc385
Chebet Kamara <chebetkamara@gmail.com>
2022-09-19 01:05:12 -0700

    Printing string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - printing string to stdout
+ * @str: string to be printed
+ *
+ * Return: none
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+}
