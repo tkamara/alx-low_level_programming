d9febae2ad364ccc403f8a1bfeba00c3722fcde0
Chebet Kamara <chebetkamara@gmail.com>
2022-09-28 07:31:20 -0700

    Printing string recursively

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string
+ * @s: string to be printed
+ *
+ * Return: none
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar(*s);
+	}
+	else
+	{
+		_puts_recursion(*s + 1);
+	}
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/**
+ * _puts_recursion - print string
+ * @s: string to be printed
+ *
+ * Return: none
+ */
+
+void _puts_recursion(char *s);
+
+int _putchar(char ch);
+
+#endif
