85d8acba62465cd62c7b64caf5ed80ef6e385e0b
Chebet Kamara <chebetkamara@gmail.com>
2022-09-28 11:03:41 -0700

    Recursion update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,6 +11,7 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
+		_putchar('\n');
 		return;
 	}
 	else
@@ -18,5 +19,4 @@ void _puts_recursion(char *s)
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
 }
