9dad059032135e58c99a5d4bdbc1494679bde178
Chebet Kamara <chebetkamara@gmail.com>
2022-10-08 08:57:30 -0700

    Function printing every other character of string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,14 +11,31 @@ void puts2(char *str)
 {
 	int counter, i;
 
-	counter = 0;
-	while (*str != '\0')
-	{
-		counter++;
-	}
+	counter = _strlen(str);
 
 	for (i = 0; i < counter; i += 2)
 	{
 		_putchar(str[i]);
 	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - function to determine length of string
+ * @s: string to determine length of
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
 }
