3d19f3513194422c9161f442aed6c123c50c2e55
Chebet Kamara <chebetkamara@gmail.com>
2022-10-08 09:36:51 -0700

    Corection for odd number of length of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,11 +13,22 @@ void puts_half(char *str)
 	int length, i;
 
 	length = _strlen(str);
-	for (i = length / 2; i < length; i++)
+	if (length % 2 == 0)
 	{
-		_putchar(str[i]);
+		for (i = length / 2; i < length; i++)
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (i = (length + 1) / 2; i < length; i++)
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
+	}
 }
 
 /**
