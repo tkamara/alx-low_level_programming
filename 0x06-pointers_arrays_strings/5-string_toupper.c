4b9671f3c16d02c2adc1acf8c9c69d876f470930
Chebet Kamara <chebetkamara@gmail.com>
2022-10-10 03:08:34 -0700

    Changing all lowercase letters to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * string_toupper - change all lowercase letters to uppercase
+ * @s: string
+ *
+ * Return: new string
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+			i++;
+		}
+		s[i] = '\0';
+	}
+
+	return (s);
+}
