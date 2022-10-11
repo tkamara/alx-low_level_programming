b13bcc4d7e5332cb919570b46db5e0e5863cb7c7
Chebet Kamara <chebetkamara@gmail.com>
2022-10-11 03:50:25 -0700

    Structs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - different information about dogs
+ * @name: name of dogs
+ * @age: age of dogs
+ * @owner: dog owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	char *age;
+	char *owner;
+}
+
+#endif
