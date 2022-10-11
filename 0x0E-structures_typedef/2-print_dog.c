704b0792d7f217b253a81b834f3ef67f31caaed5
Chebet Kamara <chebetkamara@gmail.com>
2022-10-11 04:32:44 -0700

    Correction of NULL

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -21,14 +21,7 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)\n");
 		}
 
-		if (d->age != NULL)
-		{
-			printf("Age: %.6f\n", d->age);
-		}
-		else
-		{
-			printf("Age: (nil)\n");
-		}
+		printf("Age: %.6f\n", d->age);
 
 		if (d->owner != NULL)
 		{
