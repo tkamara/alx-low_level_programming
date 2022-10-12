0f4a8b3b8ea03263c031d0d41db46e4564fa9d62
Chebet Kamara <chebetkamara@gmail.com>
2022-10-11 23:46:36 -0700

    Updating header file

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,12 +2,12 @@
 #define MAIN_H_
 
 /**
- * putchar - printing to standard output
+ * _putchar - printing to standard output
  * @ch: character to print
  *
  * Return: 1 on success
  */
 
-int putchar(char ch);
+int _putchar(char ch);
 
 #endif
