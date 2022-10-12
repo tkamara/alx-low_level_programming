aa7e177bc8650635686c1e364556ef860e5d842c
Chebet Kamara <chebetkamara@gmail.com>
2022-10-11 23:34:18 -0700

    Printing file name using argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints name of program
+ * @argc: count of arguments
+ * @argv: array of pointers to string arguments
+ *
+ * Return: zero
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/**
+ * putchar - printing to standard output
+ * @ch: character to print
+ *
+ * Return: 1 on success
+ */
+
+int putchar(char ch);
+
+#endif
