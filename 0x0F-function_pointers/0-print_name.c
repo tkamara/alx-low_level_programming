132374e8b9719e8f2324ff1c8a2a4901a9ada898
Chebet Kamara <chebetkamara@gmail.com>
2022-10-13 09:46:57 -0700

    Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: name to be printed
+ * @f: pointer to function
+ * @s: string of name
+ *
+ * Return: none
+ */
+
+void print_name(char *name, void (*f)(char *s))
+{
+	while (*name != '\0')
+	{
+		_putchar(name);
+	}
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_POINTERS_H_
+#define FUNCTION_POINTERS_H_
+
+int _putchar(char ch);
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
