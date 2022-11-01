48469f49b65d35d64779ed6bbdaabbfcab55aa48
Chebet Kamara <chebetkamara@gmail.com>
2022-11-01 12:37:52 -0700

    Correcting linter errors

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * get_bit - return value of bit at given index 
+ * get_bit - return value of bit at given index
  * @n: the number
  * @index: counter
  *
- * Return: index or -1 if error occurred 
+ * Return: index or -1 if error occurred
  */
 
 int get_bit(unsigned long int n, unsigned int index)
