c18eb56177eca5338593ec06af6190ea4c5e5e6c
Chebet Kamara <chebetkamara@gmail.com>
2023-04-12 02:21:15 -0700

    Updates

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,41 +1,6 @@
 #include "search_algos.h"
 
 /**
- * binary_search_two - function to conduct binary search
- * @array: the array to search through
- * @value: the value searched for
- * @low: the lower bound
- * @high: the upper bound
- * Return: the index of found element
- */
-
-int binary_search_two(int *array, int value, int low, int high)
-{
-	int i, mid;
-
-	while (high >= low)
-	{
-		printf("Searching through array:");
-		for (i = low; i < high; i++)
-		{
-			printf(" %d,", array[i]);
-		}
-		printf(" %d\n", array[high]);
-
-		mid = low + (high - low) / 2;
-
-		if (value == array[mid])
-			return (mid);
-		if (value > array[mid])
-			low = mid + 1;
-		else
-			high = mid - 1;
-	}
-
-	return (-1);
-}
-
-/**
  * binary_search - function to do binary search using binary_search_two
  * @array: the array to search through
  * @size: the size of the array
@@ -45,9 +10,32 @@ int binary_search_two(int *array, int value, int low, int high)
 
 int binary_search(int *array, size_t size, int value)
 {
+	int high, low, mid, i;
+
+	high = (int)size - 1;
+	low = 0;
 
 	if (array == NULL)
 		return (-1);
 
-	return (binary_search_two(array, value, 0, (int)size - 1));
+        while (high >= low)
+        {
+                printf("Searching through array:");
+                for (i = low; i < high; i++)
+                {
+                        printf(" %d,", array[i]);
+                }
+                printf(" %d\n", array[high]);
+
+                mid = low + (high - low) / 2;
+
+                if (value == array[mid])
+                        return (mid);
+                if (value > array[mid])
+                        low = mid + 1;
+                else
+                        high = mid - 1;
+        }
+
+        return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,8 +6,6 @@
 
 int linear_search(int *array, size_t size, int value);
 
-int binary_search_two(int *array, int value, int low, int high);
-
 int binary_search(int *array, size_t size, int value);
 
 #endif
