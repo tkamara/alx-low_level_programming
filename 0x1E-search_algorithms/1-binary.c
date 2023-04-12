922437126b93b44dd15b6402c437de0c958951b3
Chebet Kamara <chebetkamara@gmail.com>
2023-04-12 02:29:04 -0700

    Fixing errors

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -18,24 +18,24 @@ int binary_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-        while (high >= low)
-        {
-                printf("Searching through array:");
-                for (i = low; i < high; i++)
-                {
-                        printf(" %d,", array[i]);
-                }
-                printf(" %d\n", array[high]);
+	while (high >= low)
+	{
+		printf("Searching through array:");
+		for (i = low; i < high; i++)
+		{
+			printf(" %d,", array[i]);
+		}
+		printf(" %d\n", array[high]);
 
-                mid = low + (high - low) / 2;
+		mid = low + (high - low) / 2;
 
-                if (value == array[mid])
-                        return (mid);
-                if (value > array[mid])
-                        low = mid + 1;
-                else
-                        high = mid - 1;
-        }
+		if (value == array[mid])
+			return (mid);
+		if (value > array[mid])
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
 
-        return (-1);
+	return (-1);
 }
